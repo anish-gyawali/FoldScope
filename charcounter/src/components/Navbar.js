fb7d549550132a0a41762ccc11bf0cb1872f942c
Anish Gyawali <anish.gyawali@selu.edu>
2021-11-02 09:24:39 -0500

    added dashboard template
    It contains navbar and text area with table to show character count

---
+++ charcounter/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header class="text-white body-font">
+      <div class="container mx-auto flex flex-wrap p-5 bg-indigo-500 flex-col md:flex-row items-center ">
+        <span class="ml-3 text-xl">CHAR COUNTER</span>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
